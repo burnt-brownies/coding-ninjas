5e4059e9a7dff69071d9d96fb892a5a97fef38a5
burnt-brownies <laayeshan@gmail.com>
2026-05-20 19:55:10 +0530

    updated code

--- Conditionals and Loops/Check_If_Triangle.cpp
+++ Conditionals and Loops/Check_If_Triangle.cpp
@@ -1,4 +1,4 @@
-/* iven three positive integers as X, Y and Z representing three sides of a triangle, write a program that determines whether the triangle formed by the sides exist or not. If the triangle exists, classify it as isosceles, scalene or equilateral.
+/* Given three positive integers as X, Y and Z representing three sides of a triangle, write a program that determines whether the triangle formed by the sides exist or not. If the triangle exists, classify it as isosceles, scalene or equilateral.
 
 
 
@@ -9,25 +9,22 @@ Sum of any two of its sides should be greater than the third side */
 using namespace std;
 
 int main() {
-	int X, Y, Z;
-	cin >> X >> Y >> Z ;
+    int X, Y, Z;
+    cin >> X >> Y >> Z;
 
-	if( (X+Y) <= Z || (Y+Z) <= X || (Z+X) <= Y)
-	{
-		cout << "Not a Triangle" ;
-	}
-	else if( X==Y && Y==Z )
-	{
-		cout << "Equilateral Triangle" ;
-	}
-	else if( X==Y || Y==Z || Z==X ){
-		cout << "Isosceles Triangle" ;
-	}
-	
-	else
-	{
-		cout << "Scalene Triangle" ;
-	}
-	return 0;
-	
+    if ((X + Y) <= Z || (Y + Z) <= X || (Z + X) <= Y) {
+        cout << "Not a Triangle\n";
+        return 0;
+    }
+
+    if (X == Y && Y == Z)
+        cout << "Equilateral Triangle\n";
+    else if (X == Y || Y == Z || Z == X)
+        cout << "Isosceles Triangle\n";
+    else
+        cout << "Scalene Triangle\n";
+
+    return 0;
 }
+
+/* First checking if valid tirangle or not, if invalid returning asap and not going throught the other if else conditions, then if valid triangle checking the if else conditions, first equilateral condition, if did isoscleles condition firs, it triggers even if it is an equilateral triangle */
