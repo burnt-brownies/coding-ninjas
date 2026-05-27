8b18ea5324c6045719cc9f645f7fd4c11ba002f4
burnt-brownies <laayeshan@gmail.com>
2026-05-28 01:36:45 +0530

    Completed

---
+++ Conditionals and Loops/Square_Root.cpp
@@ -0,0 +1,18 @@
+/*
+Given a number N, find its square root. You need to find and print only the integral part of square root of N.
+For eg. if number given is 18, answer is 4.
+*/
+
+#include<iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    int i = 0;
+    while (i * i <= n) {
+        i++;
+    }
+    cout << i - 1;
+}
